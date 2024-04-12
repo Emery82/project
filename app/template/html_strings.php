a442cf41cd1327241d7b26445fd8b7dc9754fb5b
Emery82 <berek.imre@gmail.com>
2024-04-12 10:16:42 +0200

    .

--- app/template/html_strings.php
+++ app/template/html_strings.php
@@ -2,8 +2,7 @@
 
 return [
     'status_option' => '<option value="%status_id%"%selected%>%status_name%</option>',
-    'project_list_header' => '<div id="formmessage"></div>%status_options%',
-    // Add more HTML strings as needed...
+    'project_list_header' => '<div id="formmessage"></div>%status_options%',    
 ];
 
 ?>
